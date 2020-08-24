715e038edca18d296ef401774849e627aeb7ff07
Olivier Girardot <olive_girardot@hotmail.com>
2020-08-24 14:11:12 +0200

    added some training features like delete, edit count tasks and so on

--- seed/js/libs/todo.controller.js
+++ seed/js/libs/todo.controller.js
@@ -1,4 +1,11 @@
 function TodoController() {
+    this.newTodo = "";
+    this.newTodoDifficulty = "";
+    this.newTodoCategory = "";
+    this.favoriteActivity = [];
+    this.addToFavorite = item => {
+        this.favoriteActivity.push(item);
+    };
     this.list = [{
         title: 'First todo item!',
         completed: false,
@@ -19,6 +26,29 @@ function TodoController() {
         completed: true,
         difficulty: 6
     }];
+
+    this.addTodo = () => {
+        this.list.unshift({
+            title: this.newTodo,
+            difficulty: this.newTodoDifficulty,
+            category: this.newTodoCategory,
+            completed: false
+        });
+        this.newTodo = "";
+        this.newTodoCategory = "";
+        this.newTodoDifficulty = "";
+    };
+    this.removeTodo = (item, index) => {
+        this.list.splice(index, 1);
+    };
+    this.getRemaining = () => {
+        let n = 0;
+        this.list.forEach((item) => {
+            item.completed != true ? n +=1 : n += 0; 
+        });
+        return n;
+    };
+    this.time = new Date();
 }
 
 // MainController.$inject = ['$scope'];
